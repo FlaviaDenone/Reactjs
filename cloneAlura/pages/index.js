b22c24ebb73e986a4f49bdcdb6a56e6dc724c448
Flavia Denone <flavia_denone@hotmail.com>
2022-11-29 21:38:30 -0300

    Clone com Reactjs

---
+++ cloneAlura/pages/index.js
@@ -0,0 +1,65 @@
+import config from"./config.json"
+import styled from "styled-components";
+
+function HomePage() {
+    const estilosDaHomePage = {backgroundColor:"red"};
+    return (
+        <div style={estilosDaHomePage}>
+            <Menu/>
+            <Header/>
+            <Timeline/>
+        </div>
+        );
+}
+
+export default HomePage
+
+function Menu() {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}
+
+const StyledHeader = styled.div`
+    img {
+        width:80px;
+        height:80px;
+        border-radius:50%;
+    }
+    .user-info{
+        display: flex;
+        aling-itens: center;
+        width:100;
+        padding:16px 32px;
+        gap:16px;
+    }
+`;
+function Header() {
+    return (
+        <StyledHeader>
+            {/* <img src="banner"/> */}
+
+            <section className="user-info">
+            <img src={`https://github.com/${config.github}.png`}/>
+            <div>
+                <h2>
+                    {config.name}
+                 </h2>
+                 <p>
+                    {config.job}
+                </p>
+            </div>    
+            </section>
+        </StyledHeader>
+    )
+}
+
+function Timeline() {
+    return (
+        <div>
+            Timeline
+        </div>
+    )
+}
